07b1000206329485e6cad5d8a803a2a59fa1fb28
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-05-15 01:08:32 +0000

    debug statement, woops
    git-svn-id: http://partuza.googlecode.com/svn/trunk@28 329b45d6-434c-0410-81e3-f98314b225b0

--- Application/Views/profile/profile_content.php
+++ Application/Views/profile/profile_content.php
@@ -1,12 +1,12 @@
 <?
-
+/*
 $skip = array('password', 'email', 'id', 'first_name', 'last_name', 'profile_url', 'thumbnail_url');
 foreach($vars['person'] as $key => $val) {
 	if (!in_array($key,$skip) && !empty($val)) {
 		echo "<b>$key</b> $val<br />";
 	}
 }
-
+*/
 $width = 488;
 $view = 'profile';
 foreach ($vars['applications'] as $gadget) {
