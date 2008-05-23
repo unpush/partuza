8d350d5d89f37dd7225b91ee354b5632d9f81c07
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-05-23 11:02:29 +0000

    Full screen canvas view
    git-svn-id: http://partuza.googlecode.com/svn/trunk@46 329b45d6-434c-0410-81e3-f98314b225b0

--- Application/Views/applications/application_canvas.php
+++ Application/Views/applications/application_canvas.php
@@ -1,11 +1,13 @@
 <? $this->template('/common/header.php'); ?>
+<!-- 
 <div id="profileInfo" class="blue">
 	<? $this->template('profile/profile_info.php', $vars); ?>
 </div>
-<div id="profileContentWide">
+-->
+<div id="profileContentWide" style="width:'auto'; padding-left:0px;">
 <?
 $gadget = $vars['application'];
-$width = 790;
+$width = 960;
 $view = 'canvas';
 $this->template('/gadget/gadget.php', array('width' => $width, 'gadget' => $gadget, 'person' => $vars['person'], 'view' => $view));
 ?>
