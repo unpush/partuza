6f73f138dbaf5ac6a0a5184e16304669735b3098
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-06-04 10:36:18 +0000

    Added base64 fallback
    git-svn-id: http://partuza.googlecode.com/svn/trunk@56 329b45d6-434c-0410-81e3-f98314b225b0

--- Library/BasicBlobCrypter.php
+++ Library/BasicBlobCrypter.php
@@ -1,32 +1,15 @@
 <?php
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- * 
- */
 
-class BlobExpiredException extends Exception {
-}
+class BlobExpiredException extends Exception {}
 
 /**
  * This class provides basic binary blob encryption and decryption, for use with the security token
  * 
  */
 class BasicBlobCrypter extends BlobCrypter {
+	//FIXME make this compatible with the java's blobcrypter
 	
+
 	// Labels for key derivation
 	private $CIPHER_KEY_LABEL = 0;
 	private $HMAC_KEY_LABEL = 1;
@@ -44,20 +27,24 @@ class BasicBlobCrypter extends BlobCrypter {
 	
 	private $cipherKey;
 	private $hmacKey;
-	
+
 	public function __construct()
 	{
 		$this->cipherKey = Config::get('token_cipher_key');
 		$this->hmacKey = Config::get('token_hmac_key');
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public function wrap(Array $in)
 	{
 		$encoded = $this->serializeAndTimestamp($in);
-		$cipherText = Crypto::aes128cbcEncrypt($this->cipherKey, $encoded);
+		if (!function_exists('mcrypt_module_open') && Config::get('allow_plaintext_token')) {
+			$cipherText = base64_encode($encoded);
+		} else {
+			$cipherText = Crypto::aes128cbcEncrypt($this->cipherKey, $encoded);
+		}
 		$hmac = Crypto::hmacSha1($this->hmacKey, $cipherText);
 		$b64 = base64_encode($cipherText . $hmac);
 		return $b64;
@@ -66,13 +53,13 @@ class BasicBlobCrypter extends BlobCrypter {
 	private function serializeAndTimestamp(Array $in)
 	{
 		$encoded = "";
-		foreach ( $in as $key => $val ) {
+		foreach ($in as $key => $val) {
 			$encoded .= urlencode($key) . "=" . urlencode($val) . "&";
 		}
 		$encoded .= $this->TIMESTAMP_KEY . "=" . time();
 		return $encoded;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -95,22 +82,29 @@ class BasicBlobCrypter extends BlobCrypter {
 			$cipherText = substr($bin, 0, strlen($bin) - Crypto::$HMAC_SHA1_LEN);
 			$hmac = substr($bin, strlen($cipherText));
 			Crypto::hmacSha1Verify($this->hmacKey, $cipherText, $hmac);
-			$plain = Crypto::aes128cbcDecrypt($this->cipherKey, $cipherText);
+			if (!function_exists('mcrypt_module_open') && Config::get('allow_plaintext_token')) {
+				$plain = base64_decode($cipherText);
+			} else {
+				$plain = Crypto::aes128cbcDecrypt($this->cipherKey, $cipherText);
+			}
 			$out = $this->deserialize($plain);
 			$this->checkTimestamp($out, $maxAgeSec);
 		}
 		return $out;
 	}
-	
+
 	private function deserialize($plain)
 	{
 		$map = array();
 		$items = split("[&=]", $plain);
+		/*
+		//TODO: See if this can work or isn't necessary.
 		if ((count($items) / 2) != 7) {
 			// A valid token should decrypt to 14 items, aka 7 pairs.
 			// If not, this wasn't valid & untampered data and we abort
 			throw new BlobExpiredException("Invalid security token");
 		}
+		*/
 		for ($i = 0; $i < count($items); ) {
 			$key = urldecode($items[$i ++]);
 			$value = urldecode($items[$i ++]);
@@ -118,7 +112,7 @@ class BasicBlobCrypter extends BlobCrypter {
 		}
 		return $map;
 	}
-	
+
 	private function checkTimestamp(Array $out, $maxAge)
 	{
 		$minTime = (int)$out[$this->TIMESTAMP_KEY] - $this->CLOCK_SKEW_ALLOWANCE;
