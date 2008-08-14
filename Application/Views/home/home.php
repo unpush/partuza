e68bc8473ad351c15792e36b1058cca2eaeb0fad
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-08-14 23:19:50 +0000

    Make the home page a bit more useful
    git-svn-id: http://partuza.googlecode.com/svn/trunk@109 329b45d6-434c-0410-81e3-f98314b225b0

--- Application/Views/home/home.php
+++ Application/Views/home/home.php
@@ -1,8 +1,13 @@
 <? $this->template('/common/header.php'); ?>
-<img src="images/partuza-home.jpg" align="left" /> 
 <div style="float:right; width: 500px;">
-<p>Welcome to the example Social Network Site: Partuza!</p>
-<p>Please login by entering your email and password in the top or <a href="<?php echo Config::get('web_prefix') ?>/register">register</a> to continue.</p>
+<p style="color:#3366CC;font-weight:bold">Please login by entering your email and password in the top or <a href="<?php echo Config::get('web_prefix') ?>/register">register</a> to continue.</p>
+<p>Partuza is an example Open Social - Social Network Site that uses <a href="http://incubator.apache.org/shindig/">Apache Shindig.</a></p>
+<p>The goals of Partuza are to: </p>
+<ul><li>Allow open social gadget developers to develop quickly and in private on their local, open social compliant servers. </li><li>Serve as an example of how to implement open social support using shindig in your own social site. </li></ul>
+<p>For people already comfortable with how to set up a website there are 2 quick guides to get started: <ul><li><a href="http://code.google.com/p/partuza/wiki/GettingStarted">http://code.google.com/p/partuza/wiki/GettingStarted</a> </li><li><a href="http://code.google.com/p/partuza/wiki/SettingUpShindigAndPartuza">http://code.google.com/p/partuza/wiki/SettingUpShindigAndPartuza</a> </li></ul></p>
+<p>There are 2 simple-to-follow guides for getting shindig and partuza up and running on a windows pc at: <ul><li><a href="http://chabotc.com/guides/shindig_install/">http://chabotc.com/guides/shindig_install/</a> </li><li><a href="http://chabotc.com/guides/partuza_install/" rel="nofollow">http://chabotc.com/guides/partuza_install/</a> </li></ul></p>
+<p>And we have a developers and support mailing list up at: <ul><li><a href="http://groups.google.com/group/partuza">http://groups.google.com/group/partuza</a></li></ul></p>
 </div>
-
+<br />
+<img src="images/partuza-home.jpg" align="left" /> 
 <? $this->template('/common/footer.php'); ?>
