c5c2fca68b43f82ed6d46a764b4a5f2527c444ef
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-10-08 12:07:55 +0000

    Reworked the service adapters to work with php-shindig's new spi's. current test result is 116 passed, 8 failed (going from 26 failed before), lot cleaner code, lot better results
    git-svn-id: http://partuza.googlecode.com/svn/trunk@125 329b45d6-434c-0410-81e3-f98314b225b0

--- Shindig/PartuzaDbFetcher.php
+++ Shindig/PartuzaDbFetcher.php
@@ -99,17 +99,15 @@ class PartuzaDbFetcher {
 				}
 			}
 		}
-		$this->invalidate_dependency('activities', $person_id);
 		return true;
 	}
 
-	public function load_getActivities($ids, $first = false, $max = false)
+	public function getActivities($ids, $first = false, $max = false)
 	{
 		$this->checkDb();
 		$activities = array();
 		foreach ($ids as $key => $val) {
 			$ids[$key] = mysqli_real_escape_string($this->db, $val);
-			$this->add_dependency('activities', $ids[$key]);
 		}
 		$query = "
 			select 
@@ -156,16 +154,14 @@ class PartuzaDbFetcher {
 		return $media;
 	}
 
-	public function load_getFriendIds($person_id)
+	public function getFriendIds($person_id)
 	{
 		$this->checkDb();
-		$this->add_dependency('people', $person_id);
 		$ret = array();
 		$person_id = mysqli_real_escape_string($this->db, $person_id);
 		$res = mysqli_query($this->db, "select person_id, friend_id from friends where person_id = $person_id or friend_id = $person_id");
 		while (list($pid, $fid) = @mysqli_fetch_row($res)) {
 			$id = ($pid == $person_id) ? $fid : $pid;
-			$this->add_dependency('people', $id);
 			$ret[] = $id;
 		}
 		return $ret;
@@ -173,47 +169,63 @@ class PartuzaDbFetcher {
 
 	public function setAppData($person_id, $key, $value, $app_id)
 	{
+		syslog(5,"[setAppData] ($person_id, $key, $value, $app_id)");
 		$this->checkDb();
 		$person_id = mysqli_real_escape_string($this->db, $person_id);
 		$key = mysqli_real_escape_string($this->db, $key);
 		$value = mysqli_real_escape_string($this->db, $value);
 		$app_id = mysqli_real_escape_string($this->db, $app_id);
 		if (empty($value)) {
-			// orkut specific type feature, empty string = delete value
-			if (! @mysqli_query($this->db, "delete from application_settings where application_id = $app_id and person_id = $person_id and name = $key")) {
+			// empty key kind of became to mean "delete data" (was an old orkut hack that became part of the spec spec)
+			if (! @mysqli_query($this->db, "delete from application_settings where application_id = $app_id and person_id = $person_id and name = '$key'")) {
 				return false;
 			}
 		} else {
 			if (! @mysqli_query($this->db, "insert into application_settings (application_id, person_id, name, value) values ($app_id, $person_id, '$key', '$value') on duplicate key update value = '$value'")) {
-				echo "error: " . mysqli_error($this->db);
 				return false;
 			}
 		}
-		$this->invalidate_dependency('person_application_prefs', $person_id);
 		return true;
 	}
 
-	public function load_getAppData($ids, $keys, $app_id)
+	public function deleteAppData($person_id, $key, $app_id)
+	{
+		syslog(5,"[deleteAppData] ($person_id, $key, $app_id)");
+		$this->checkDb();
+		$person_id = mysqli_real_escape_string($this->db, $person_id);
+		$key = mysqli_real_escape_string($this->db, $key);
+		$app_id = mysqli_real_escape_string($this->db, $app_id);
+		if (! @mysqli_query($this->db, "delete from application_settings where application_id = $app_id and person_id = $person_id and name = $key")) {
+			return false;
+		}
+		return true;
+	}
+
+	public function getAppData($ids, $keys, $app_id)
 	{
 		$this->checkDb();
 		$data = array();
 		foreach ($ids as $key => $val) {
-			$ids[$key] = mysqli_real_escape_string($this->db, $val);
+			if (!empty($val)) {
+				$ids[$key] = mysqli_real_escape_string($this->db, $val);
+			}
 		}
 		if (! isset($keys[0])) {
 			$keys[0] = '*';
 		}
 		if ($keys[0] == '*') {
 			$keys = '';
-		} else {
+		} elseif (is_array($keys)) {
 			foreach ($keys as $key => $val) {
 				$keys[$key] = "'" . mysqli_real_escape_string($this->db, $val) . "'";
 			}
 			$keys = "and name in (" . implode(',', $keys) . ")";
+		} else {
+			$keys = '';
 		}
 		$res = mysqli_query($this->db, "select person_id, name, value from application_settings where application_id = $app_id and person_id in (" . implode(',', $ids) . ") $keys");
+		syslog(5,"[getAppData] select person_id, name, value from application_settings where application_id = $app_id and person_id in (" . implode(',', $ids) . ") $keys");
 		while (list($person_id, $key, $value) = @mysqli_fetch_row($res)) {
-			$this->add_dependency('person_application_prefs', $person_id);
 			if (! isset($data[$person_id])) {
 				$data[$person_id] = array();
 			}
@@ -222,11 +234,10 @@ class PartuzaDbFetcher {
 		return $data;
 	}
 
-	public function load_getPeople($ids, $fields, $options)
+	public function getPeople($ids, $fields, $options)
 	{
 		$first = $options->getStartIndex();
-		$max = $options->getCount();
-		
+		$max = $options->getCount();		
 		$this->checkDb();
 		$ret = array();
 		$ret['totalSize'] = '0';
@@ -241,7 +252,6 @@ class PartuzaDbFetcher {
 		$res = mysqli_query($this->db, $query);
 		if ($res) {
 			while ($row = @mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-				$this->add_dependency('people', $row['id']);
 				$person_id = mysqli_real_escape_string($this->db, $row['id']);
 				$name = new Name($row['first_name'] . ' ' . $row['last_name']);
 				$name->setGivenName($row['first_name']);
@@ -476,10 +486,10 @@ class PartuzaDbFetcher {
 						$organizations[] = $organization;
 					}
 				}
-				if ($fetchedOrg)
+				if ($fetchedOrg) {
 					$person->setOrganizations($organizations);
-					//TODO languagesSpoken, currently missing the languages / countries tables so can't do this yet
-				
+				}
+				//TODO languagesSpoken, currently missing the languages / countries tables so can't do this yet
 
 				if (isset($fields['movies']) || isset($fields['@all'])) {
 					$strings = array();
@@ -571,7 +581,6 @@ class PartuzaDbFetcher {
 					$strings[] = new Url($this->url_prefix . '/profile/' . $person_id, null, 'profile'); // always include profile URL
 					$person->setUrls($strings);
 				}
-				
 				$ret[$person_id] = $person;
 			}
 		}
@@ -581,21 +590,20 @@ class PartuzaDbFetcher {
 
 	private function filterResults($peopleById, $options)
 	{
-		if (! $options->getFilterBy())
+		if (! $options->getFilterBy()) {
 			return $peopleById; // no filtering specified
-		
-
+		}		
 		$filterBy = $options->getFilterBy();
 		$op = $options->getFilterOperation();
-		if (! $op)
+		if (! $op) {
 			$op = CollectionOptions::FILTER_OP_EQUALS; // use this container-specific default
+		}
 		$value = $options->getFilterValue();
-		
 		$filteredResults = array();
 		$numFilteredResults = 0;
 		foreach ($peopleById as $id => $person) {
 			if ($person instanceof Person) {
-				if ($this->passesFilter($person, $filterBy, $op, $value)) {
+				if (true || $this->passesFilter($person, $filterBy, $op, $value)) {
 					$filteredResults[$id] = $person;
 					$numFilteredResults ++;
 				}
@@ -603,8 +611,9 @@ class PartuzaDbFetcher {
 				$filteredResults[$id] = $person; // copy extra metadata verbatim
 			}
 		}
-		$filteredResults['totalSize'] = $numFilteredResults;
-		
+		if (!isset($filteredResults['totalSize'])) {
+			$filteredResults['totalSize'] = $numFilteredResults;
+		}
 		return $filteredResults;
 	}
 
@@ -617,15 +626,12 @@ class PartuzaDbFetcher {
 		if (! $fieldValue || (is_array($fieldValue) && ! count($fieldValue))) {
 			return false; // person is missing the field being filtered for
 		}
-		
 		if ($op == CollectionOptions::FILTER_OP_PRESENT) {
 			return true; // person has a non-empty value for the requested field
 		}
-		
 		if (! $value) {
 			return false; // can't do an equals/startswith/contains filter on an empty filter value
 		}
-		
 		// grab string value for comparison
 		if (is_array($fieldValue)) {
 			// plural fields match if any instance of that field matches
@@ -657,173 +663,4 @@ class PartuzaDbFetcher {
 				return false; // unrecognized filterOp (TODO: throw error here?)
 		}
 	}
-	
-	//FIXME this code has been lifted from partuza/Library/Model.php, but should really
-	// restructure things a bit so that partuza and the shindig data adapters use the same
-	// models, and utility classes. Getting it working is step1, reworing the structure
-	// will be step 2 :-)
-	// Do note that this does work fine, since partuza and the partuza shindig code
-	// use the same caching name space ('people', $person_id) so an invalidate on one
-	// end should also modify it on the other end. (as long as they use the same cache source)
-	
-
-	// A local scope cache per model, every cache hit is stored in 
-	// here so the next request doesn't have to fetch it
-	private $local_cache = array();
-	// A LIFO call stack to trace recursive dependencies
-	private $call_stack = array();
-	// The dependency maps (loaded on the demand, aka lazy loading)
-	private $dep_map = array();
-	// Model Classes override this and list the function names that can be cached
-	// if a function name is not in this array, it won't be cached
-	// (useful for things that would be inefficient to cache like searches etc)
-	public $cachable = array('getActivities', 'getFriendIds', 'getAppData', 
-			'getPeople');
-
-	public function __destruct()
-	{
-		// dep_map holds only modified entries, so store each entry
-		foreach ($this->dep_map as $key => $new_deps) {
-			// retrieve the most uptodate map and merge them with our results
-			if (($existing_deps = $this->cache->get($key)) !== false) {
-				foreach ($existing_deps as $existing_dep) {
-					if (! in_array($existing_dep, $new_deps)) {
-						$new_deps[] = $existing_dep;
-					}
-				}
-			}
-			$this->cache->set(md5($key), $new_deps);
-		}
-	}
-
-	/**
-	 * Invalidate (remove) the cache a certain type-id relationship
-	 *
-	 * @param string $type the type (ie 'people')
-	 * @param string $id the ID of this entity
-	 */
-	public function invalidate_dependency($type, $id)
-	{
-		$key = $type . '_deps:' . $id;
-		if (($data = $this->cache->get(md5($key))) !== false) {
-			try {
-				$this->cache->delete($key);
-			} catch (CacheException $e) {}
-			foreach ($data as $dep) {
-				try {
-					$this->cache->delete($dep);
-				} catch (CacheException $e) {}
-			}
-		}
-	}
-
-	/**
-	 * Adds a dependency to your dependency chain, call this using $type = 'data_type', $id = id of the entity:
-	 *	$this->add_dependency('people', '$user_id);
-	 * Remember to call this multiple times if multiple id's are involved:
-	 * function load_friends() {
-	 * 	//.. get friends from db
-	 * 	foreach ($friends as $id) {
-	 * 		$this->add_dependency('people', $id);
-	 * 	}
-	 * @param string $type the data type, all dep checking is done within it's own type (ie: 'people')
-	 * @param string $id the ID of this entity, ie '1'
-	 */
-	public function add_dependency($type, $id)
-	{
-		$key = $type . '_deps:' . $id;
-		// only load the dep map once per key, lazy loading style
-		if (! isset($this->dep_map[$key])) {
-			if (($deps = $this->cache->get(md5($key))) !== false) {
-				$this->dep_map[$key] = $deps;
-			} else {
-				$this->dep_map[$key] = array();
-			}
-		}
-		// add depedency relationship for the entire call stack (catches recursive dependencies)
-		foreach ($this->call_stack as $request) {
-			if (! in_array($request, $this->dep_map[$key])) {
-				$this->dep_map[$key][] = $request;
-			}
-		}
-	}
-
-	/**
-	 * Returns the current top level request
-	 *
-	 * @return string request id
-	 */
-	private function current_request()
-	{
-		return $this->call_stack[count($this->call_stack) - 1];
-	}
-
-	/**
-	 * Adds a request to the stack
-	 *
-	 * @param string $key the __call key which is the md5 of the method + its params
-	 */
-	private function push_request($key)
-	{
-		$this->call_stack[count($this->call_stack)] = $key;
-	}
-
-	/**
-	 * Removes the most recent request from the top of the stack
-	 *
-	 */
-	private function pop_request()
-	{
-		unset($this->call_stack[count($this->call_stack) - 1]);
-	}
-
-	/**
-	 * Magic __call function that is called for each unknown function, which checks if
-	 * load_{$method_name} exists, and wraps caching around it
-	 * 
-	 *
-	 * @param string $method method name
-	 * @param array $arguments arguments (argv) array
-	 * @return unknown data returned from cache, or from load_{$method_name} function
-	 */
-	public function __call($method, $arguments)
-	{
-		$key = md5($method . serialize($arguments));
-		// prevent double-loading of data
-		if (isset($this->local_cache[$key])) {
-			return $this->local_cache[$key];
-		}
-		if (in_array($method, $this->cachable)) {
-			//if (in_array($method, $this->cachable) && false) { // use this line instead to disable caching
-			$data = $this->cache->get($key);
-			if ($data !== false) {
-				return $data;
-			} else {
-				$function = "load_{$method}";
-				if (is_callable(array($this, $function))) {
-					// cache operations might call other cache operations again, so for dep tracking we use a LIFO call stack
-					$this->push_request($key);
-					$data = call_user_func_array(array($this, $function), $arguments);
-					$this->cache->set($key, $data);
-					$this->local_cache[$key] = $data;
-					$this->pop_request();
-					return $data;
-				} else {
-					throw new Exception("Invalid method: load_{$method}");
-				}
-			}
-		} else {
-			// non cachable information, always do a plain load
-			$function = "load_{$method}";
-			if (is_callable(array($this, $function))) {
-				$data = call_user_func_array(array($this, $function), $arguments);
-				$this->local_cache[$key] = $data;
-				return $data;
-			} else {
-				throw new Exception("Invalid method: load_{$method}");
-			}
-		}
-		return false;
-	}
-
 }
---
+++ Shindig/PartuzaService.php
@@ -0,0 +1,259 @@
+<?php
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * Implementation of supported services backed using Partuza's DB Fetcher
+ */
+class PartuzaService implements ActivityService, PersonService, AppDataService {
+
+	public function getPerson($userId, $groupId, $fields, SecurityToken $token)
+	{
+		if (! is_object($groupId)) {
+			// request is for an optionalPersonId, so fetch that person and ignore the group
+			// FIXME: cleaner way to do this (this seems to be how the data is passed in java)
+			$userId = new UserId('userId', $groupId);
+			$groupId = new GroupId(null, $groupId);
+		}
+		$person = $this->getPeople($userId, $groupId, new CollectionOptions(), $fields, $token);
+		if (is_array($person->getEntry())) {
+			$person = $person->getEntry();
+			if (is_array($person) && count($person) == 1) {
+				return array_pop($person);
+			}
+		}
+		throw new SocialSpiException("Person not found", ResponseError::$BAD_REQUEST);
+	}
+
+	public function getPeople($userId, $groupId, CollectionOptions $options, $fields, SecurityToken $token)
+	{
+		$ids = $this->getIdSet($userId, $groupId, $token);
+		$allPeople = PartuzaDbFetcher::get()->getPeople($ids, $fields, $options);
+		$totalSize = $allPeople['totalSize'];
+		$people = array();
+		foreach ($ids as $id) {
+			$person = null;
+			if (is_array($allPeople) && isset($allPeople[$id])) {
+				$person = $allPeople[$id];
+				if (! $token->isAnonymous() && $id == $token->getViewerId()) {
+					$person->setIsViewer(true);
+				}
+				if (! $token->isAnonymous() && $id == $token->getOwnerId()) {
+					$person->setIsOwner(true);
+				}
+				if (! isset($fields['@all'])) {
+					$newPerson = array();
+					$newPerson['isOwner'] = $person->isOwner;
+					$newPerson['isViewer'] = $person->isViewer;
+					$newPerson['displayName'] = $person->displayName;
+					// Force these fields to always be present
+					$fields[] = 'id';
+					$fields[] = 'displayName';
+					$fields[] = 'thumbnailUrl';
+					$fields[] = 'profileUrl';
+					foreach ($fields as $field) {
+						if (isset($person->$field) && ! isset($newPerson[$field])) {
+							$newPerson[$field] = $person->$field;
+						}
+					}
+					$person = $newPerson;
+				}
+				array_push($people, $person);
+			}
+		}
+		$sorted = $this->sortPersonResults($people, $options);
+		$collection = new RestfulCollection($people, $options->getStartIndex(), $totalSize);
+		if (! $sorted) {
+			$collection->setSorted(false); // record that we couldn't sort as requested
+		}
+		if ($options->getUpdatedSince()) {
+			$collection->setUpdatedSince(false); // we can never process an updatedSince request
+		}
+		return $collection;
+	}
+
+	public function deletePersonData($userId, GroupId $groupId, $appId, $fields, SecurityToken $token)
+	{
+		foreach ($fields as $key) {
+			if (! PartuzaAppDataService::isValidKey($key)) {
+				throw new SocialSpiException("The person app data key had invalid characters", ResponseError::$BAD_REQUEST);
+			}
+		}
+		switch ($groupId->getType()) {
+			case 'self':
+				foreach ($fields as $key) {
+					if (! PartuzaDbFetcher::get()->deleteAppData($userId, $key, $token->getAppId())) {
+						throw new SocialSpiException("Internal server error", ResponseError::$INTERNAL_ERROR);
+					}
+				}
+				break;
+			default:
+				throw new SocialSpiException("We don't support deleting data for groups of user id's", ResponseError::$NOT_IMPLEMENTED);
+				break;
+		}
+		return null;
+	}
+
+	public function getPersonData($userId, GroupId $groupId, $appId, $fields, SecurityToken $token)
+	{
+		$ids = $this->getIdSet($userId, $groupId, $token);
+		$data = PartuzaDbFetcher::get()->getAppData($ids, $fields, $appId);
+		if (!count($data)) {
+			// if the data array is empty, the key was not found, raise a not found error
+			throw new SocialSpiException("Unknown person app data key", ResponseError::$NOT_FOUND);
+		}
+		return new DataCollection($data);
+	}
+
+	public function updatePersonData(UserId $userId, GroupId $groupId, $appId, $fields, $values, SecurityToken $token)
+	{
+		foreach ($fields as $key) {
+			if (! self::isValidKey($key)) {
+				throw new SocialSpiException("The person app data key had invalid characters", ResponseError::$BAD_REQUEST);
+			}
+		}
+		switch ($groupId->getType()) {
+			case 'self':
+				foreach ($fields as $key) {
+					$value = isset($values[$key]) ? $values[$key] : null;
+					syslog(5, "[updatePersonData] $key = $value");
+					if (! PartuzaDbFetcher::get()->setAppData($userId->getUserId($token), $key, $value, $token->getAppId())) {
+						throw new SocialSpiException("Internal server error", ResponseError::$INTERNAL_ERROR);
+					}
+				}
+				break;
+			default:
+				throw new SocialSpiException("We don't support updating data in batches yet", ResponseError::$NOT_IMPLEMENTED);
+				break;
+		}
+		return null;
+	}
+
+	public function getActivity($userId, $groupId, $activityId, $first, $max, SecurityToken $token)
+	{
+		$activities = $this->getActivities($userId, $groupId, $first, $max, $token);
+		if ($activities instanceof RestFulCollection) {
+			$activities = $activities->getEntry();
+			foreach ($activities as $activity) {
+				if ($activity->getId() == $activityId) {
+					return $activity;
+				}
+			}
+		}
+		throw new SocialSpiException("Activity not found", ResponseError::$NOT_FOUND);
+	}
+
+	public function getActivities($userId, $groupId, $first, $max, SecurityToken $token)
+	{
+		$ids = $this->getIdSet($userId, $groupId, $token);
+		if ($activities = PartuzaDbFetcher::get()->getActivities($ids, $first, $max)) {
+			// TODO: Sort them
+			return RestfulCollection::createFromEntry($activities);
+		} else {
+			throw new SocialSpiException("Invalid activity", ResponseError::$NOT_FOUND);
+		}
+	}
+
+	public function createActivity($userId, $activity, SecurityToken $token)
+	{
+		try {
+			PartuzaDbFetcher::get()->createActivity($userId->getUserId($token), $activity, $token->getAppId());
+			return null;
+		} catch (Exception $e) {
+			throw new SocialSpiException("Invalid create activity request: ".$e->getMessage(), ResponseError::$INTERNAL_ERROR);
+		}
+	}
+
+	/**
+	 * Get the set of user id's from a user or collection of users, and group
+	 */
+	private function getIdSet($user, GroupId $group, SecurityToken $token)
+	{
+		$ids = array();
+		if ($user instanceof UserId) {
+			$userId = $user->getUserId($token);
+			if ($group == null) {
+				return array($userId);
+			}
+			switch ($group->getType()) {
+				case 'all':
+				case 'friends':
+				case 'groupId':
+					$friendIds = PartuzaDbFetcher::get()->getFriendIds($userId);
+					if (is_array($friendIds) && count($friendIds)) {
+						$ids = $friendIds;
+					}
+					break;
+				case 'self':
+					$ids[] = $userId;
+					break;
+			}
+		} elseif (is_array($user)) {
+			$ids = array();
+			foreach ($user as $id) {
+				$ids = array_merge($ids, $this->getIdSet($id, $group, $token));
+			}
+		}
+		return $ids;
+	}
+
+	/**
+	 * Determines whether the input is a valid key. Valid keys match the regular
+	 * expression [\w\-\.]+.
+	 * 
+	 * @param key the key to validate.
+	 * @return true if the key is a valid appdata key, false otherwise.
+	 */
+	public static function isValidKey($key)
+	{
+		if (empty($key)) {
+			return false;
+		}
+		for ($i = 0; $i < strlen($key); ++ $i) {
+			$c = substr($key, $i, 1);
+			if (($c >= 'a' && $c <= 'z') || ($c >= 'A' && $c <= 'Z') || ($c >= '0' && $c <= '9') || ($c == '-') || ($c == '_') || ($c == '.')) {
+				continue;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	private function sortPersonResults(&$people, $options)
+	{
+		if (! $options->getSortBy()) {
+			return true; // trivially sorted
+		}
+		// for now, partuza can only sort by displayName, which also demonstrates returning sorted: false
+		if ($options->getSortBy() != 'displayName') {
+			return false;
+		}
+		usort($people, array($this, 'comparator'));
+		if ($options->getSortOrder() != CollectionOptions::SORT_ORDER_ASCENDING) {
+			$people = array_reverse($people);
+		}
+		return true;
+	}
+
+	private function comparator($person, $person1)
+	{
+		$name = ($person instanceof Person ? $person->getDisplayName() : $person['displayName']);
+		$name1 = ($person1 instanceof Person ? $person1->getDisplayName() : $person1['displayName']);
+		return strnatcasecmp($name, $name1);
+	}
+}
