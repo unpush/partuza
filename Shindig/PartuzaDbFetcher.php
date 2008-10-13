39f2efbc690936a43f0ee5b5e18a372150866e43
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-10-13 21:41:51 +0000

    Re-add caching to PartuzaDbFetcher, when debug = true caching will now be disabled, and on debug = false caching will be enabled (since it's a huge performance gain)
    git-svn-id: http://partuza.googlecode.com/svn/trunk@133 329b45d6-434c-0410-81e3-f98314b225b0

--- Shindig/PartuzaDbFetcher.php
+++ Shindig/PartuzaDbFetcher.php
@@ -22,6 +22,18 @@ class PartuzaDbFetcher {
 	private $db;
 	private $url_prefix;
 	private $cache;
+	// A local scope cache per model, every cache hit is stored in
+	// here so the next request doesn't have to fetch it
+	private $local_cache = array();
+	// A LIFO call stack to trace recursive dependencies
+	private $call_stack = array();
+	// The dependency maps (loaded on the demand, aka lazy loading)
+	private $dep_map = array();
+	// Model Classes override this and list the function names that can be cached
+	// if a function name is not in this array, it won't be cached
+	// (useful for things that would be inefficient to cache like searches etc)
+	public $cachable = array('getActivities', 'getFriendIds', 'getAppData', 
+			'getPeople');
 	
 	// Singleton
 	private static $fetcher;
@@ -40,6 +52,22 @@ class PartuzaDbFetcher {
 		$this->cache = new $cache();
 		// change this to your site's location
 		$this->url_prefix = 'http://partuza';
+	}
+
+	public function __destruct()
+	{
+		// dep_map holds only modified entries, so store each entry
+		foreach ($this->dep_map as $key => $new_deps) {
+			// retrieve the most uptodate map and merge them with our results
+			if (($existing_deps = $this->cache->get($key)) !== false) {
+				foreach ($existing_deps as $existing_dep) {
+					if (! in_array($existing_dep, $new_deps)) {
+						$new_deps[] = $existing_dep;
+					}
+				}
+			}
+			$this->cache->set(md5($key), $new_deps);
+		}
 	}
 
 	private function checkDb()
@@ -99,15 +127,17 @@ class PartuzaDbFetcher {
 				}
 			}
 		}
+		$this->invalidate_dependency('activities', $person_id);
 		return true;
 	}
 
-	public function getActivities($ids, $appId, $sortBy, $filterBy, $startIndex, $count, $fields)
+	public function load_getActivities($ids, $appId, $sortBy, $filterBy, $startIndex, $count, $fields)
 	{
 		$this->checkDb();
 		$activities = array();
 		foreach ($ids as $key => $val) {
 			$ids[$key] = mysqli_real_escape_string($this->db, $val);
+			$this->add_dependency('activities', $ids[$key]);
 		}
 		$ids = implode(',', $ids);
 		// return a proper totalResults count
@@ -117,8 +147,8 @@ class PartuzaDbFetcher {
 		} else {
 			$totalResults = '0';
 		}
-		$startIndex = (!is_null($startIndex) && $startIndex !== false && is_numeric($startIndex)) ? intval($startIndex) : '0';
-		$count = (!is_null($count) && $count !== false && is_numeric($count)) ? intval($count) : '20';
+		$startIndex = (! is_null($startIndex) && $startIndex !== false && is_numeric($startIndex)) ? intval($startIndex) : '0';
+		$count = (! is_null($count) && $count !== false && is_numeric($count)) ? intval($count) : '20';
 		$activities['totalResults'] = $totalResults;
 		$activities['startIndex'] = $startIndex;
 		$activities['count'] = $count;
@@ -154,12 +184,13 @@ class PartuzaDbFetcher {
 			return false;
 		}
 	}
-	
+
 	public function deleteActivities($userId, $appId, $activityIds)
 	{
 		$this->checkDb();
 		foreach ($activityIds as $key => $val) {
 			$activityIds[$key] = mysqli_real_escape_string($this->db, $val);
+			$this->invalidate_dependency('activities', $val);
 		}
 		$activityIds = implode(',', $activityIds);
 		$userId = mysqli_real_escape_string($this->db, $userId);
@@ -179,9 +210,10 @@ class PartuzaDbFetcher {
 		return $media;
 	}
 
-	public function getFriendIds($person_id)
+	public function load_getFriendIds($person_id)
 	{
 		$this->checkDb();
+		$this->add_dependency('people', $person_id);
 		$ret = array();
 		$person_id = mysqli_real_escape_string($this->db, $person_id);
 		$res = mysqli_query($this->db, "select person_id, friend_id from friends where person_id = $person_id or friend_id = $person_id");
@@ -209,6 +241,7 @@ class PartuzaDbFetcher {
 				return false;
 			}
 		}
+		$this->invalidate_dependency('appdata', $person_id);
 		return true;
 	}
 
@@ -221,16 +254,18 @@ class PartuzaDbFetcher {
 		if (! @mysqli_query($this->db, "delete from application_settings where application_id = $app_id and person_id = $person_id and name = '$key'")) {
 			return false;
 		}
+		$this->invalidate_dependency('appdata', $person_id);
 		return true;
 	}
 
-	public function getAppData($ids, $keys, $app_id)
+	public function load_getAppData($ids, $keys, $app_id)
 	{
 		$this->checkDb();
 		$data = array();
 		foreach ($ids as $key => $val) {
-			if (!empty($val)) {
+			if (! empty($val)) {
 				$ids[$key] = mysqli_real_escape_string($this->db, $val);
+				$this->add_dependency('appdata', $ids[$key]);
 			}
 		}
 		if (! isset($keys[0])) {
@@ -256,10 +291,13 @@ class PartuzaDbFetcher {
 		return $data;
 	}
 
-	public function getPeople($ids, $fields, $options)
+	public function load_getPeople($ids, $fields, $options)
 	{
+		foreach ($ids as $personId) {
+			$this->add_dependency('people', $personId);
+		}
 		$first = $options->getStartIndex();
-		$max = $options->getCount();		
+		$max = $options->getCount();
 		$this->checkDb();
 		$ret = array();
 		$ret['totalSize'] = '0';
@@ -512,6 +550,7 @@ class PartuzaDbFetcher {
 					$person->setOrganizations($organizations);
 				}
 				//TODO languagesSpoken, currently missing the languages / countries tables so can't do this yet
+				
 
 				if (isset($fields['movies']) || isset($fields['@all'])) {
 					$strings = array();
@@ -614,7 +653,7 @@ class PartuzaDbFetcher {
 	{
 		if (! $options->getFilterBy()) {
 			return $peopleById; // no filtering specified
-		}		
+		}
 		$filterBy = $options->getFilterBy();
 		$op = $options->getFilterOperation();
 		if (! $op) {
@@ -633,7 +672,7 @@ class PartuzaDbFetcher {
 				$filteredResults[$id] = $person; // copy extra metadata verbatim
 			}
 		}
-		if (!isset($filteredResults['totalSize'])) {
+		if (! isset($filteredResults['totalSize'])) {
 			$filteredResults['totalSize'] = $numFilteredResults;
 		}
 		return $filteredResults;
@@ -683,6 +722,145 @@ class PartuzaDbFetcher {
 				return strpos($fieldValue, $filterValue) === 0;
 			default:
 				return false; // unrecognized filterOp (TODO: throw error here?)
+		}
+	}
+	
+	/*
+	 * Caching functions ... uses php's automagic call functions to overload function
+	 * names, if a function is named load_<functionName> and the function name is
+	 * listed in the $cachable array, the results of it are cached based on the
+	 * md5 of the params parsed to it.
+	 * Caching is DISABLED when debug = true, and ENABLED if debug = false
+	 * 
+	 * it uses the add_dependency / invalidate_depencency functions to track
+	 * dependencies between objects.
+	 */
+
+	/**
+	 * Invalidate (remove) the cache a certain type-id relationship
+	 *
+	 * @param string $type the type (ie 'people')
+	 * @param string $id the ID of this entity
+	 */
+	public function invalidate_dependency($type, $id)
+	{
+		$key = $type . '_deps:' . $id;
+		if (($data = $this->cache->get(md5($key))) !== false) {
+			try {
+				$this->cache->delete($key);
+			} catch (CacheException $e) {}
+			foreach ($data as $dep) {
+				try {
+					$this->cache->delete($dep);
+				} catch (CacheException $e) {}
+			}
+		}
+	}
+
+	/**
+	 * Adds a dependency to your dependency chain, call this using $type = 'data_type', $id = id of the entity:
+	 *      $this->add_dependency('people', '$user_id);
+	 * Remember to call this multiple times if multiple id's are involved:
+	 * function load_friends() {
+	 *      //.. get friends from db
+	 *      foreach ($friends as $id) {
+	 *              $this->add_dependency('people', $id);
+	 *      }
+	 * @param string $type the data type, all dep checking is done within it's own type (ie: 'people')
+	 * @param string $id the ID of this entity, ie '1'
+	 */
+	public function add_dependency($type, $id)
+	{
+		$key = $type . '_deps:' . $id;
+		// only load the dep map once per key, lazy loading style
+		if (! isset($this->dep_map[$key])) {
+			if (($deps = $this->cache->get(md5($key))) !== false) {
+				$this->dep_map[$key] = $deps;
+			} else {
+				$this->dep_map[$key] = array();
+			}
+		}
+		// add depedency relationship for the entire call stack (catches recursive dependencies)
+		foreach ($this->call_stack as $request) {
+			if (! in_array($request, $this->dep_map[$key])) {
+				$this->dep_map[$key][] = $request;
+			}
+		}
+	}
+
+	/**
+	 * Returns the current top level request
+	 *
+	 * @return string request id
+	 */
+	private function current_request()
+	{
+		return $this->call_stack[count($this->call_stack) - 1];
+	}
+
+	/**
+	 * Adds a request to the stack
+	 *
+	 * @param string $key the __call key which is the md5 of the method + its params
+	 */
+	private function push_request($key)
+	{
+		$this->call_stack[count($this->call_stack)] = $key;
+	}
+
+	/**
+	 * Removes the most recent request from the top of the stack
+	 *
+	 */
+	private function pop_request()
+	{
+		unset($this->call_stack[count($this->call_stack) - 1]);
+	}
+
+	/**
+	 * Magic __call function that is called for each unknown function, which checks if
+	 * load_{$method_name} exists, and wraps caching around it
+	 *
+	 *
+	 * @param string $method method name
+	 * @param array $arguments arguments (argv) array
+	 * @return unknown data returned from cache, or from load_{$method_name} function
+	 */
+	public function __call($method, $arguments)
+	{
+		$key = md5($method . serialize($arguments));
+		// prevent double-loading of data
+		if (isset($this->local_cache[$key])) {
+			return $this->local_cache[$key];
+		}
+		if (in_array($method, $this->cachable) && !Config::get('debug')) {
+			$data = $this->cache->get($key);
+			if ($data !== false) {
+				return $data;
+			} else {
+				$function = "load_{$method}";
+				if (is_callable(array($this, $function))) {
+					// cache operations might call other cache operations again, so for dep tracking we use a LIFO call stack
+					$this->push_request($key);
+					$data = call_user_func_array(array($this, $function), $arguments);
+					$this->cache->set($key, $data);
+					$this->local_cache[$key] = $data;
+					$this->pop_request();
+					return $data;
+				} else {
+					throw new Exception("Invalid method: load_{$method}");
+				}
+			}
+		} else {
+			// non cachable information, always do a plain load
+			$function = "load_{$method}";
+			if (is_callable(array($this, $function))) {
+				$data = call_user_func_array(array($this, $function), $arguments);
+				$this->local_cache[$key] = $data;
+				return $data;
+			} else {
+				throw new Exception("Invalid method: load_{$method}");
+			}
 		}
 	}
 }
