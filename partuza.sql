2d264c0f24a67cafa5beb3f19036c0e868063da7
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2008-12-11 16:10:58 +0000

    sync up db dump with new approved column
    git-svn-id: http://partuza.googlecode.com/svn/trunk@176 329b45d6-434c-0410-81e3-f98314b225b0

--- partuza.sql
+++ partuza.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: partuza
 -- ------------------------------------------------------
--- Server version	5.0.51a
+-- Server version	5.0.67
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -115,6 +115,7 @@ CREATE TABLE `applications` (
   `version` varchar(64) NOT NULL,
   `height` int(11) NOT NULL default '0',
   `scrolling` int(11) NOT NULL default '0',
+  `approved` enum('Y','N') default 'N',
   `modified` int(11) NOT NULL,
   UNIQUE KEY `url` (`url`),
   KEY `id` (`id`)
@@ -655,4 +656,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-11-08 17:33:09
+-- Dump completed on 2008-12-11 16:08:57
