9f86445cf1065662b8c43ecc42b5f3b31234928b
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2009-01-08 23:22:05 +0000

    now with proper zulu formatting
    git-svn-id: http://partuza.googlecode.com/svn/trunk@182 329b45d6-434c-0410-81e3-f98314b225b0

--- Library/XRDS.php
+++ Library/XRDS.php
@@ -21,7 +21,7 @@
 $xrds = '<XRDS xmlns="xri://$xrds">
     <XRD xml:id="oauth" xmlns:simple="http://xrds-simple.net/core/1.0" xmlns="xri://$XRD*($v*2.0)" version="2.0">
       <Type>xri://$xrds*simple</Type>
-      <Expires>'.date('YmdHiZ', time() + 30 * 24 * 60 * 60).'Z</Expires>
+      <Expires>'.date('Y-m-d\TH:i:s\Z', time() + 30 * 24 * 60 * 60).'</Expires>
       <Service priority="10">
         <Type>http://oauth.net/core/1.0/endpoint/request</Type>
         <Type>http://oauth.net/core/1.0/parameters/auth-header</Type>
