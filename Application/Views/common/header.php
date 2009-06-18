d525d13be5d8ba4dbf4d65221609e4d5c44996b8
chabotc <chabotc@329b45d6-434c-0410-81e3-f98314b225b0>
2009-06-18 06:29:07 +0000

    Add the osapi.js container side code to support the osapi rpc methods
    git-svn-id: http://partuza.googlecode.com/svn/trunk@239 329b45d6-434c-0410-81e3-f98314b225b0

--- Application/Views/common/header.php
+++ Application/Views/common/header.php
@@ -6,6 +6,7 @@
 <link href="<?=PartuzaConfig::get('web_prefix')?>/css/container.css?v=5" rel="stylesheet" type="text/css">
 <link type="text/css" href="<?=PartuzaConfig::get('web_prefix')?>/css/jquery.css?v=5" rel="Stylesheet" />
 <script type="text/javascript" src="<?=PartuzaConfig::get('gadget_server')?>/gadgets/js/rpc.js?c=1"></script>
+<script type="text/javascript" src="<?=PartuzaConfig::get('gadget_server')?>/gadgets/files/container/osapi.js"></script>
 <!--  the below was concated and compressed with yuicompressor using: java -jar {$path}/yuicompressor-2.3.5.jar -o {$file}-min.js {$file}.js -->
 <!--  script type="text/javascript" src="<?=PartuzaConfig::get('web_prefix')?>/js/jquery-1.3.js"></script>
 <script type="text/javascript" src="<?=PartuzaConfig::get('web_prefix')?>/js/jquery.ui.all.js"></script>
